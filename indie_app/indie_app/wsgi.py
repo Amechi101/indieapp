5afed2a00bdfdaca6be748ac01441631ea1381d3
Amechi Egbe <egbe@usc.edu>
2015-02-02 19:52:26 -0500

    changed wsgi

--- indie_app/indie_app/wsgi.py
+++ indie_app/indie_app/wsgi.py
@@ -17,8 +17,8 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indie_app.staging_settings")
 
-import indie_app.startup as startup
-startup.run()
+# import indie_app.startup as startup
+# startup.run()
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
--- indie_app/manage.py
+++ indie_app/manage.py
@@ -5,8 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indie_app.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indie_app.staging_settings")
     from django.core.management import execute_from_command_line
     import indie_app.startup as startup
     startup.run()
+    
     execute_from_command_line(sys.argv)
