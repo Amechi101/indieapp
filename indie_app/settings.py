18e639c256a917b2270c20be7a8d732a86c95efd
Amechi Egbe <egbe@usc.edu>
2015-02-02 23:15:05 -0500

    heroku db settings

--- indie_app/settings.py
+++ indie_app/settings.py
@@ -12,23 +12,20 @@ PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql", # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         "NAME": "heroku_560a6be42009a00", #devlocal.db for sqlite3
-#         'USER': 'ba0b3876b2b57d',
-#         'PASSWORD': 'da519b20',
-#         'HOST': 'https://salty-refuge-1024.herokuapp.com',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3", # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        "NAME": os.path.join(PROJECT_ROOT, 'devlocal.db') #devlocal.db for sqlite3
+        "ENGINE": "django.db.backends.postgresql_psycopg2", # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        "NAME": "", #devlocal.db for sqlite3
+
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3", # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         "NAME": os.path.join(PROJECT_ROOT, 'devlocal.db') #devlocal.db for sqlite3
+#     }
+# }
 
 
 # Local time zone for this installation. Choices can be found here:
@@ -280,16 +277,16 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
 
 ############## Heroku ########################
 
-# # Parse database configuration from $DATABASE_URL
-# if not os.environ.has_key('DATABASE_URL'):
-#     os.environ['DATABASE_URL'] = 'mysql://ba0b3876b2b57d:da519b20@us-cdbr-iron-east-01.cleardb.net/heroku_560a6be42009a00?reconnect=true'
+# Parse database configuration from $DATABASE_URL
+if not os.environ.has_key('DATABASE_URL'):
+    os.environ['DATABASE_URL'] = 'postgres://fpyfzfpjxajdbh:XinpGlrb5jifObW6zdz70WS6o2@ec2-23-21-235-249.compute-1.amazonaws.com:5432/da0jlh34a082ck'
 
-# import dj_database_url
-# DATABASES['heroku_560a6be42009a00'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
