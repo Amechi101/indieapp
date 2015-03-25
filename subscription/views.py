bf6d80c61b002640f1ccfa102972c4691b88ebe6
nyddle <nyddle@gmail.com>
2015-03-25 22:31:55 +0300

    Update views.py

--- subscription/views.py
+++ subscription/views.py
@@ -21,32 +21,22 @@ class LoginRequiredMixin(object):
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
-@login_required
-def account_subscribe( request ):   
-  
-	brands = Subscription.objects.filter(brands=request.user)
-	return render_to_response("account/account_features/_user_brands.html", {"brands": brands})
 
 
-@login_required
-def subscribe( request ):
-	SubscriptionManager().subscribe(brand=get_object_or_404(Brand, brand_name=request.GET.get("brand_name") ),user=request.user)
-
-	if request.GET.get("ajax"):
-		return HttpResponse('ok')
-	return HttpResponseRedirect("/account/subscribed-brands/?added=")
-
-
-@login_required
-def unsubscribe( request ):
-	SubscriptionManager.unsubscribe(brand=get_object_or_404(name=request.GET.get("brand_name"), user=request.user))
-	return HttpResponse("ok")
 
 
 class SubscriptionListView(LoginRequiredMixin, View):	
 	def get(self, request):
 		brands = Subscription.objects.filter(brands=request.user)
 		return render_to_response("account/account_features/_user_brands.html", {"brands": brands})
+		
+class SubscribeView(LoginRequiredMixin, View):
+	def get(self, request):
+		SubscriptionManager().subscribe(brand=get_object_or_404(Brand, brand_name=request.GET.get("brand_name") ),user=request.user)
+
+		if request.GET.get("ajax"):
+			return HttpResponse('ok')
+		return HttpResponseRedirect("/account/subscribed-brands/?added=")
 
 class UnsubscribeView(LoginRequiredMixin, View):	
 	def get(self, request):
