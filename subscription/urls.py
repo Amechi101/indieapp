e1a090f71319f7a7badc07a34df9e311cbaada9b
nyddle <nyddle@gmail.com>
2015-03-25 22:33:02 +0300

    Update urls.py

--- subscription/urls.py
+++ subscription/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls import patterns, include, url
 
 from . import views
 
-from subscription.views import account_subscribe, unsubscribe, subscribe, SubscriptionListView, UnsubscribeView
+from subscription.views import account_subscribe, unsubscribe, subscribe, \
+			SubscriptionListView, UnsubscribeView, SubscribeView
 
 
 urlpatterns = patterns('',
 
-	url(r"^api/subscribe/", views.subscribe),
+	url(r"^api/subscribe/", SubscribeView.as_view()),
 	url(r"^api/unsubscribe/", UnsubscribeView.as_view()),
 	url(r"^subscribed-brands/$", SubscriptionListView.as_view() ),
 )
