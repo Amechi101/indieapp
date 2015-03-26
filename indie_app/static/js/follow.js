05951dfc92fef2d67ff988f8040bea208cb8558d
Alexander Davydov <nyddle@gmail.com>
2015-03-26 21:02:46 +0300

    adding ajax

--- indie_app/static/js/follow.js
+++ indie_app/static/js/follow.js
@@ -1,1 +1,20 @@
-alert('Im followjs');
+$('unlabelFollow').click(e) {
+   e.preventDefault();
+  alert('followed...');
+  // <a id="unlabelFollow" href="/account/api/subscribe/?brand_name={{ brand.brand_name }}"><span class=" brand-follow icon-unlabel_web-05"></span></a>
+  var link = $('#unlabelFollow').attr('href');
+  alert(link);
+  $.ajax({
+        type: "GET",
+        url: link,
+        success: function(r) {
+            alert(r);
+            if (r.status == "ok") {
+            } else {
+                alert(r.error);
+            }
+        }
+    });
+
+}
+
