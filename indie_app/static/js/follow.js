01a8f3e46864b8ae813afadda30b59bd1dfc2498
Alexander Davydov <nyddle@gmail.com>
2015-03-29 00:38:30 +0300

    Update follow.js

--- indie_app/static/js/follow.js
+++ indie_app/static/js/follow.js
@@ -21,7 +21,20 @@ $(document).ready(function(){
     });
 
 
-       $.ajax({
+ 
+
+
+    $('.brand_follow').click(function(e) {
+    	
+    	e.preventDefault();
+    	alert('followed...');
+
+    	var link = $('.brand_follow').attr('href');
+
+    	link += '&ajax=1';
+		alert(link);
+		
+      $.ajax({
             type: "GET",
             url: link,
             success: function(r) {
@@ -46,36 +59,6 @@ $(document).ready(function(){
             }
         });
     });
-/*
-
-    $('.brand_follow').click(function(e) {
-    	
-    	e.preventDefault();
-    	alert('followed...');
-
-    	var link = $('.brand_follow').attr('href');
-
-    	link += '&ajax=1';
-		alert(link);
-		
-		$.ajax({
-		    type: "GET",
-		    url: link,
-		    success: function(r) {
-		        console.log(r);
-		        if (r.status == "ok") {
-		        	if ($(this).find('span').hasClass('follow')) {
-		        	          //str.replace(regexp, newSubStr|function)
-		        	          link.replace('subscribe', 'unsubscribe');
-		        	          $('.brand_follow').attr('href', link)
-                        $(this).find('span').removeClass('follow')
-                        $(this).find('span').addClass('unfollow')
-                    } else {
-                        link.replace('subscribe', 'unsubscribe');
-		        	          $('.brand_follow').attr('href', link)
-                        $(this).find('span').removeClass('unfollow')
-                        $(this).find('span').addClass('follow')
-                    }
 
 
 });
